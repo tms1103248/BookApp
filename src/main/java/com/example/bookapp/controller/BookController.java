a24b1f5858900b7977aed0b8fd41183018671860
Шишков <tms1103248@gmail.com>
2023-03-27 15:43:44 +0300

    27.03.2023

--- src/main/java/com/example/bookapp/controller/BookController.java
+++ src/main/java/com/example/bookapp/controller/BookController.java
@@ -10,37 +10,37 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@Slf4j
-//@RestController()
-//@RequestMapping("/books")
-//@RequiredArgsConstructor
-//public class BookController {
-//
-//    private final BookService bookService;
-//
-//
-//    @GetMapping("/{id}")
-//    public Book getBookById(@PathVariable Long id) {
-//        return bookService.getBookById(id);
-//    }
-//
-//
-//    @GetMapping
-//    public List<Book> getAllBooks(@RequestParam(required = false) String author) {
-//        if (author != null)
-//            return bookService.findByAuthor(author);
-//
-//        return bookService.getAllBooks();
-//    }
-//
-//    @PostMapping
-//    public void addBook(@RequestBody BookRequest request) {
-//        bookService.addBook(mapper.AddBookRequestToBook(request));
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void editBook(@PathVariable Long id, @RequestBody BookRequest request) {
-//        bookService.editBook(mapper.EditBookRequestToBook(id, request));
-//    }
-//}
+@Slf4j
+@RestController()
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+
+    @GetMapping
+    public List<Book> getAllBooks(@RequestParam(required = false) String author) {
+        if (author != null)
+            return bookService.findByAuthor(author);
+
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody BookRequest request) {
+        bookService.addBook(mapper.AddBookRequestToBook(request));
+    }
+
+    @PutMapping("/{id}")
+    public void editBook(@PathVariable Long id, @RequestBody BookRequest request) {
+        bookService.editBook(mapper.EditBookRequestToBook(id, request));
+    }
+}
 
