161c53d7b0455ebc7a44ee34c1f53a06cab05848
Шишков <tms1103248@gmail.com>
2023-04-01 20:23:35 +0300

    01.04.2023

---
+++ src/main/java/com/example/bookapp/controller/DockerController.java
@@ -0,0 +1,34 @@
+package com.example.bookapp.controller;
+
+
+import com.example.bookapp.entity.Author;
+import com.example.bookapp.repository.DockerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("test")
+public class DockerController {
+    private final DockerRepository dockerRepository;
+
+    @GetMapping
+    public List<Author> findAll(){
+        return dockerRepository.findAll();
+    }
+
+    @PostMapping
+    public void create (@RequestBody String comment) {
+
+        Author author = new Author();
+        author.setName(comment);
+        author.setSurname(comment);
+        dockerRepository.save(author);
+//        с фронта приняли данные и сохранили в базу
+    }
+
+
+
+}
--- src/main/java/com/example/bookapp/repository/DockerRepository.java
+++ src/main/java/com/example/bookapp/repository/DockerRepository.java
@@ -1,10 +1,10 @@
 package com.example.bookapp.repository;
 
 
-import com.example.bookapp.entity.Book;
+import com.example.bookapp.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ComposeRepository extends JpaRepository<Book,Long> {
+public interface DockerRepository extends JpaRepository<Author,Long> {
 }
